7089ab5ddf80e58fac1bd5bc757c5430c4215b53
abhirup1998 <abhidash@outlook.com>
2018-01-09 02:54:31 +0530

    First Commit

---
+++ CV.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as pl
+cv2.__version__
+#img2 = cv2.imread('watch.jpg', 1)
+#cv2.imshow('image', img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
